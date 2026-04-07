e57e45f4f04c721c5e52a8cacebeabca8f27143f
Your Name <your_email@example.com>
2026-04-07 17:09:22 +0200

    fix_photosize

--- services/api/src/app.ts
+++ services/api/src/app.ts
@@ -44,7 +44,7 @@ app.use(helmet({
 }));
 app.use(cors({ origin: config.corsOrigin, credentials: true }));
 app.use(morgan('tiny'));
-app.use(express.json({ limit: '12mb' }));
+app.use(express.json({ limit: '20mb' }));
 
 app.use(privacyRouter);
 
